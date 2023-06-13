1d1e294a85eafea0893c24082369983743c47ff7
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-13 15:33:29 -0300

    style - New gradient background and app title changed to "Formulário Multi Steps".

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -7,10 +7,30 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 }
 
+body {
+    background: linear-gradient(90deg, #1D8841, #633BBC);
+    background-size: 200% 200%;
+    animation: gradientAnimation 20s ease-in-out infinite;
+}
+
+@keyframes gradientAnimation {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
 section {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
 }
-`
+`;
